ea0b934ea16c2fcd0e0f9d0489782e7530cc4f84
saldulander <tomysaldutto@hotmail.com>
2016-05-22 18:46:55 -0300

    another changes, products controller, routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+get "product/:permalink", to: "products#show", as: "product"
+post "product/:permalink", to: "products#buy", as: "buy"
+root to: "products#index"
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
